f62d4e1b3e9c516854cdc755ab40bed3664470fe
Titus Faulkner Tull <titus@MacBookAir.cable.rcn.com>
2025-05-02 16:15:02 -0400

    added bullet groups

--- src/game/main.ts
+++ src/game/main.ts
@@ -8,23 +8,24 @@ type Player = {
   inputKeys?:
     | Phaser.Types.Input.Keyboard.CursorKeys
     | { [key: string]: Phaser.Input.Keyboard.Key };
+  pointer?: Phaser.Input.Pointer;
 };
 
 type Enemy = {
-  sprite: Phaser.GameObjects.Sprite;
+  sprite: Phaser.Physics.Arcade.Sprite;
   moveSpeed: number;
   health: number;
 };
 
-type Bullet = {
-  sprite: Phaser.GameObjects.Sprite;
+type Bullet = Phaser.Physics.Arcade.Sprite & {
   damage: number;
+  moveSpeed: number;
 };
 
 type GameState = {
   player: Player;
-  enemies: Enemy[];
-  bullets: Bullet[];
+  enemies: Phaser.GameObjects.Group;
+  bullets: Phaser.GameObjects.Group;
 };
 
 let gameState: GameState;
@@ -48,17 +49,28 @@ function create(this: Phaser.Scene) {
       moveSpeed: 200,
       health: 100,
     },
-    enemies: [],
-    bullets: [],
+    enemies: this.physics.add.group({}),
+    bullets: this.physics.add.group({
+      createCallback: (bullet) => {
+        configureBullet(bullet as Bullet);
+      },
+    }),
   };
 
-  //INPUTS
+  //KEY Inputs
   gameState.player.inputKeys = this.input.keyboard?.addKeys({
     up: Phaser.Input.Keyboard.KeyCodes.W,
     down: Phaser.Input.Keyboard.KeyCodes.S,
     left: Phaser.Input.Keyboard.KeyCodes.A,
     right: Phaser.Input.Keyboard.KeyCodes.D,
   }) as { [key: string]: Phaser.Input.Keyboard.Key };
+  //MOUSE Inputs
+  gameState.player.pointer = this.input.activePointer;
+  this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
+    if (pointer.leftButtonDown()) {
+      createBullet(gameState);
+    }
+  });
 }
 //////////
 
@@ -91,6 +103,40 @@ function movePlayer(player: Player) {
   }
   player.sprite.setVelocity(inputVec.x, inputVec.y);
 }
+
+function createBullet(gameState: GameState) {
+  const player = gameState.player.sprite;
+  const pointer = gameState.player.pointer;
+  const bullet = gameState.bullets.get(player.x, player.y, "bullet") as Bullet;
+
+  if (bullet) {
+    bullet.setActive(true);
+    bullet.setVisible(true);
+
+    if (pointer) {
+      const angle = Phaser.Math.Angle.Between(
+        player.x,
+        player.y,
+        pointer.x,
+        pointer.y
+      );
+      bullet.setRotation(angle);
+      bullet.setVelocity(
+        Math.cos(angle) * bullet.moveSpeed,
+        Math.sin(angle) * bullet.moveSpeed
+      );
+    }
+  }
+}
+
+//HELPER FUNCTIONS//////// allows me to pass types to groups without classes
+function configureBullet(bullet: Bullet) {
+  bullet.damage = 10;
+  bullet.moveSpeed = 300;
+}
+//////////
+
+//////////////
 
 //Game Configs//////////
 const config: Phaser.Types.Core.GameConfig = {
